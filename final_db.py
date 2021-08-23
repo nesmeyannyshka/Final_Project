39e5914eb9be8b26037dd1b57ad23d750e5784fa
nesmeyannyshka <semenyuk_a@lwo.by>
2021-08-23 19:51:17 +0300

    v.1.0

--- final_db.py
+++ final_db.py
@@ -14,14 +14,14 @@ default_args = {
 def return_tables():
     return ('orders','products','departments','aisles','clients','stores','store_types','location_areas')
 
-'''
+
 def load_to_bronze_group(value):
     return PythonOperator(
         task_id="load_"+value+"_to_bronze",
         python_callable=load_to_bronze_spark,
         op_kwargs={"table": value}
     )
-'''
+
 def load_to_silver_group(value):
     return PythonOperator(
         task_id="load_"+value+"_to_silver",
@@ -36,7 +36,7 @@ dag = DAG(
     start_date=datetime(2021, 8, 22,23,59),
     default_args=default_args
 )
-'''
+
 dummy1 = DummyOperator(
     task_id='start_load_to_bronze',
     dag=dag
@@ -51,11 +51,11 @@ dummy3 = DummyOperator(
     task_id='start_load_to_silver',
     dag=dag
 )
-'''
+
 dummy4 = DummyOperator(
     task_id='finish_load_to_silver',
     dag=dag
 )
 
 for table in return_tables():
-    load_to_silver_group(table) >> dummy4
+    dummy1>>load_to_bronze_group(table) >> dummy2>> dummy3 >> load_to_silver_group(table) >> dummy4
